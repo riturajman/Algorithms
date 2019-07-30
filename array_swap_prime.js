dc0d1c241783542450580bc20c9c382534634373
riturajman <riturajmandal17@outlook.com>
2019-07-30 19:42:44 +0530

    After updating readline-sycn

--- array_swap_prime.js
+++ array_swap_prime.js
@@ -1,64 +1,62 @@
-function factor(x){
-    let myArray = []
-    for(let i = 2; i <= Math.sqrt(x); i++){
-        if(x % i == 0){
-            myArray.push(i);
-            if(i !== x/i){
-            myArray.push(x/i)
-            }
-        }
+var readlineSync = require('readline-sync');
+var arr1 = JSON.parse(readlineSync.question("Enter array 1: "))
+var arr2 = JSON.parse(readlineSync.question("Enter array 2: "))
+function factor(x) {
+  let myArray = [];
+  for (let i = 2; i <= Math.sqrt(x); i++) {
+    if (x % i == 0) {
+      myArray.push(i);
+      if (i !== x / i) {
+        myArray.push(x / i);
+      }
     }
-    return(myArray)
-    
+  }
+  return myArray;
 }
 
-
-function isPrime(x){
-    let arr = factor(x)
-    if(x === 1){
-        return false;
-    }
-    else if(arr[0] == null){
-        return true;
-    }
-    else{
-        return false;
-    }
+function isPrime(x) {
+  let arr = factor(x);
+  if (x === 1 || x === 0) {
+    return false;
+  } else if (arr[0] == null) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function check(array){
-    returnArray = []
-    for(i = 0; i < array.length; i++){
-        if(isPrime(array[i]) === true){
-            returnArray.push(array[i])
-        }
+function check(array) {
+  returnArray = [];
+  for (i = 0; i < array.length; i++) {
+    if (isPrime(array[i]) === true) {
+      returnArray.push(array[i]);
     }
-    return returnArray;
+  }
+  return returnArray;
 }
 
 function swapPrime(array1, array2) {
-    array1 = check(array1)
-    array2 = check(array2)
-    let m = array1.length, n = array2.length
+  array1 = check(array1);
+  array2 = check(array2);
+  let m = array1.length,
+    n = array2.length;
   if (array1.length > array2.length) {
     a = array1.length;
   } else {
     a = array2.length;
   }
   for (i = 0; i < a; i++) {
-    if(array1[i] !== undefined && array2[i] !== undefined){
-        array1[i] = array1[i] ^ array2[i];
-        array2[i] = array1[i] ^ array2[i];
-        array1[i] = array1[i] ^ array2[i];
-    }
-    else if(array1[i] !== undefined && array2[i] === undefined){
-        array2[i] = array1[i]
-    }
-    else{
-        array1[i] = array2[i]
+    if (array1[i] !== undefined && array2[i] !== undefined) {
+      array1[i] = array1[i] ^ array2[i];
+      array2[i] = array1[i] ^ array2[i];
+      array1[i] = array1[i] ^ array2[i];
+    } else if (array1[i] !== undefined && array2[i] === undefined) {
+      array2[i] = array1[i];
+    } else {
+      array1[i] = array2[i];
     }
   }
-  console.log(array1.slice(0, n), array2.slice(0, m));
+  console.log("Array 1:", array1.slice(0, n));
+  console.log("Array 2:", array2.slice(0, m))
 }
-
-swapPrime([1, 2, 10], [5, 6, 7, 8, 9, 12, 15]);
+swapPrime(arr1, arr2)
