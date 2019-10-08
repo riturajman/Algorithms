9667a60cef3536fd1c852a96f07184d3a5fee094
riturajman <riturajmandal17@outlook.com>
2019-10-08 13:53:29 +0530

    print 12345 to 1

---
+++ print1.js
@@ -0,0 +1,21 @@
+//algorith to print the following pattern\
+//12345
+//1234
+//123
+//12
+//1
+
+print= (x)=>{
+    arr = []
+    for(i= 1; i <= x; i++){
+        arr.push(i)
+    }
+    console.log(arr.join(''))
+}
+
+(design = () => {
+    for (x = 5; x >= 1; x--) {
+        print(x)
+    }
+})()
+
