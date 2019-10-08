5a36fdc109f3be5869cebf1713e9c2f07e195fe2
riturajman <riturajmandal17@outlook.com>
2019-10-08 17:12:47 +0530

    print 12344321 to 1******1

---
+++ print3.js
@@ -0,0 +1,36 @@
+//algorithm to print
+//12344321
+//123**321
+//12****21
+//1******1
+
+//takes in an array and adds its reverse to the end
+//input -> 123 ; oputput -> 123321
+printReverse = (arr)=>{
+    newArr = [arr]
+    for(i= 3; i >= 0; i--){
+        newArr.push(arr[i])
+    }
+    console.log(newArr.join(''))
+}
+
+//prints the first half of the desired pattern
+print = (x) => {
+    arr = []
+    for (i = 1; i <= 4; i++) {
+        if (i > x) {
+            arr.push("*")
+        } else {
+            arr.push(i)
+        }
+    }
+    printReverse(arr.join(''))
+}
+
+(design = () => {
+    x = 4
+    do {
+        print(x)
+        x--
+    } while (x >= 1);
+})()
